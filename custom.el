dfbe519a2a234dd3c5ddfd88972869c40a6e3c77
mikeyford <mfogeo@gmail.com>
2018-11-19 14:52:56 +0000

    first commit

---
+++ custom.el
@@ -0,0 +1,66 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "ec5f697561eaf87b1d3b087dd28e61a2fc9860e4c862ea8e6b0b77bd4967d0ba" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" "6950cff91f52578d46e0c3c0b68d329a72157cca1e2380e2e8e7557b8257eb6d" default)))
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(package-selected-packages
+   (quote
+    (osx-trash csv-mode flycheck-joker flymake-python-pyflakes transpose-frame org-plus-contrib magit-gh-pulls all-the-icons-dired dired-sidebar leuven-theme deft clj-refactor visual-fill-column jedi markdown-mode dired-hide-dotfiles rainbow-delimiters clojure-mode exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring beacon anzu ace-window)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(safe-local-variable-values
+   (quote
+    ((flycheck-disabled-checkers emacs-lisp-checkdoc)
+     (pyvenv-activate . "~/anaconda3/envs/witan.ap"))))
+ '(scroll-bar-mode nil)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil)
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "pink1"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "khaki2"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "DarkSeaGreen1"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "CadetBlue1"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "plum"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "RosyBrown1"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "DarkOliveGreen2"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "LightYellow1"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "CadetBlue1"))))
+ '(rainbow-delimiters-mismatched-face ((t (:inherit rainbow-delimiters-unmatched-face))))
+ '(rainbow-delimiters-unmatched-face ((t (:foreground "Red")))))
---
+++ my_init.el
@@ -0,0 +1,115 @@
+(prelude-require-packages '(visual-fill-column
+                            clj-refactor deft
+                            ;leuven-theme
+                            ;dired-sidebar
+                            ;all-the-icons-dired
+                            ;magit-gh-pulls
+                            ;slime
+                            xml-rpc
+                            metaweblog
+                            ;htmlize
+                            ;terraform-mode
+                            ;dockerfile-mode
+                            puppet-mode
+                            counsel-projectile
+                            ;company-terraform
+                            company-shell
+                            ag
+                            ;leuven-theme
+                            ;;ob-async
+                            ;fold-dwim
+                            ;fold-dwim-org
+                            ;org2blog
+                            ;deft
+                            ;all-the-icons
+                            ;all-the-icons-dired
+                            ;;all-the-icons-ivy
+                            ;;doom-themes
+                            ;;leuven-theme
+                            ;pivotal-tracker
+                            ;;org-gcal
+                            ;;unicode-fonts
+                            ;browse-at-remote
+                            ;;calfw
+                            ;;calfw-org
+                            ;smart-mode-line
+                            ;;smart-mode-line-powerline-theme
+                            neotree
+                            ;pinentry
+                            ;;twittering-mode
+                            ;mustache-mode
+                            ;ipcalc
+                            ;ob-async
+                            pyvenv
+                            ;ein
+                            ;nyan-mode
+
+                            ))
+
+
+;;make arrows work in tmate
+(defadvice terminal-init-screen
+    ;; The advice is named `tmux', and is run before `terminal-init-screen' runs.
+    (before tmux activate)
+  ;; Docstring.  This describes the advice and is made available inside emacs;
+  ;; for example when doing C-h f terminal-init-screen RET
+  "Apply xterm keymap, allowing use of keys passed through tmux."
+  ;; This is the elisp code that is run before `terminal-init-screen'.
+  (if (getenv "TMUX")
+      (let ((map (copy-keymap xterm-function-map)))
+        (set-keymap-parent map (keymap-parent input-decode-map))
+        (set-keymap-parent input-decode-map map))))
+
+;; theme
+;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'zenburn t)
+
+;; turn on modes
+(diff-hl-margin-mode 1)
+(diff-hl-flydiff-mode 1)
+;(hs-minor-mode 1)
+
+;; rainbow brackets
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+
+;; font
+(set-face-attribute 'default nil
+                    :family "Inconsolata" :height 145 :weight 'bold)
+
+;; clojure
+(require 'flycheck-joker)
+(setq cljr-warn-on-eval nil)
+
+;; python
+(add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "--simple-prompt -i")
+(pyvenv-tracking-mode 1)
+;(require 'flymake-python-pyflakes)
+;(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+;(setq flymake-python-pyflakes-executable "flake8")
+
+
+;turn off some prelude stuff
+(setq prelude-guru nil)
+(setq prelude-whitespace nil)
+
+;;delete to trash
+(when (eq system-type 'darwin)
+  (osx-trash-setup))
+(setq delete-by-moving-to-trash t)
+
+;; key  bindings
+(global-set-key (kbd "C-x C-r") 'recentf-open-files)
+(global-set-key (kbd "C-c [") 'diff-hl-revert-hunk)
+
+
+(defun dired-mode-key-bindings-hook ()
+  (local-set-key (kbd "C-x <up>") 'dired-up-directory)
+  (local-set-key (kbd "C-x C-<up>") 'dired-up-directory)
+  (local-set-key (kbd ".") 'dired-hide-dotfiles-mode)
+  (local-set-key (kbd "C-<up>") (kbd "C-u 10 <up>"))
+  (local-set-key (kbd "C-<down>") (kbd "C-u 10 <down>")))
+
+(add-hook 'dired-mode-hook 'dired-mode-key-bindings-hook)
